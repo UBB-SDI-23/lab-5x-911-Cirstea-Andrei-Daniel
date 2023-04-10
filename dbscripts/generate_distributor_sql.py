bde8b01ef96b79981feebd643163b1ff33f991e2
TheRealANDREWQA <andreicfsteaua@gmail.com>
2023-04-10 13:41:21 +0300

    [13] Distributor generation script

---
+++ dbscripts/generate_distributor_sql.py
@@ -0,0 +1,61 @@
+if __name__ == '__main__':
+    from faker import Faker
+    fake = Faker()
+    
+    COUNT = 1000000
+    
+    countries = fake.random_elements(elements=
+        ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 
+             'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 
+             'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 
+             'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 
+             'Cape Verde', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo', 
+             'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 
+             'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 
+             'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 
+             'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 
+             'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 
+             'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 
+             'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 
+             'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 
+             'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 
+             'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 
+             'Niger', 'Nigeria', 'North Korea', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 
+             'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 
+             'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 
+             'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 
+             'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia'
+        ], length=COUNT, unique=False)
+    
+    categories = fake.random_elements(
+        elements=["NewCars", "OldCars", "RenovatedCars"],
+        length=COUNT, unique=False)
+    
+    sql_start_string = "INSERT INTO distributor (distributorid, name, contact_email, country, cooperation_start_date, category) VALUES\n"
+
+    # create list to store SQL statements
+    lines = []
+
+    # generate fake data and create INSERT SQL statements
+    for i in range(COUNT):
+        name = fake.name()
+        date = fake.date()
+        contact_email = fake.email()
+        id = i
+        
+        if (i % 10000) == 0:
+            print(i)
+        
+        lines.append("('{}', '{}', '{}', '{}', '{}', '{}')".format(id, name, contact_email, countries[i], date, categories[i]))
+        if ((i + 1) % 1000) != 0:
+            lines[i] += ",\n"
+
+    # write SQL statements to file in /tmp directory
+    with open(r'insert_distributor.sql', 'w') as f:
+        iteration_count = int(COUNT / 1000)
+        for i in range(iteration_count):
+            f.write(sql_start_string)
+            offset = i * 1000
+            for j in range(0, 1000):
+                f.write(lines[offset + j])
+            f.write(";")
