02353d0ba1a48b18778a8b680c945c2abb99e9a3
TheRealANDREWQA <andreicfsteaua@gmail.com>
2023-05-06 13:23:55 +0300

    Statistic retry

--- src/main/java/com/example/mpp1/Repository/CarModelRepository.java
+++ src/main/java/com/example/mpp1/Repository/CarModelRepository.java
@@ -1,6 +1,9 @@
 package com.example.mpp1.Repository;
 
 import com.example.mpp1.Model.CarModelStatisticDTO;
+import jakarta.persistence.ColumnResult;
+import jakarta.persistence.ConstructorResult;
+import jakarta.persistence.SqlResultSetMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.mpp1.Model.CarModel;
@@ -12,8 +15,26 @@ import java.util.List;
 @Repository
 public interface CarModelRepository extends JpaRepository<CarModel, Long> {
 
-    @Query("SELECT c.id, c.model, c.manufacturer, c.price, c.manufacture_year, c.fuel_consumption, COUNT(p) AS purchaseCount" +
-            " FROM CarModel c LEFT JOIN c.carsOnPurchaseList p GROUP BY c.id")
+    @Query(value = "SELECT cm.id, cm.model, cm.manufacturer, cm.price, cm.manufacture_year, cm.fuel_consumption, COUNT(cop.id) as purchaseCount " +
+            "FROM CarModel cm LEFT JOIN CarsOnPurchase cop ON cm.id = cop.car_model_id ",
+            nativeQuery = true)
     List<CarModelStatisticDTO> getCarModelsWithPurchaseCount();
+
+    @SqlResultSetMapping(
+            name = "CarModelStatisticDTOMapping",
+            classes = @ConstructorResult(
+                    targetClass = CarModelStatisticDTO.class,
+                    columns = {
+                            @ColumnResult(name = "id", type = Long.class),
+                            @ColumnResult(name = "model", type = String.class),
+                            @ColumnResult(name = "manufacturer", type = String.class),
+                            @ColumnResult(name = "price", type = Long.class),
+                            @ColumnResult(name = "manufacture_year", type = Long.class),
+                            @ColumnResult(name = "fuel_consumption", type = Long.class),
+                            @ColumnResult(name = "purchaseCount", type = Integer.class)
+                    }
+            )
+    )
+    static interface CarModelStatisticDTOProjection {}
 
 }
