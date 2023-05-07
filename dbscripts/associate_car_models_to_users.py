31e8252360d665717170212d239ebb50be05cc0c
TheRealANDREWQA <andreicfsteaua@gmail.com>
2023-05-07 13:25:34 +0300

    Associate python scripts for sql modification

---
+++ dbscripts/associate_car_models_to_users.py
@@ -0,0 +1,4 @@
+import associate_to_users
+
+if __name__ == '__main__':
+    associate_to_users.associate(1000000, "update_car_model_users.sql", "car_model", "carid")
---
+++ dbscripts/associate_cars_on_purchases_to_users.py
@@ -0,0 +1,4 @@
+import associate_to_users
+
+if __name__ == '__main__':
+    associate_to_users.associate(10000000, "update_cars_on_purchases_users.sql", "cars_on_purchase", "cars_on_purchaseid")
