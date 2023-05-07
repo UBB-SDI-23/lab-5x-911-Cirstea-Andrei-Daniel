bb81256b0dded8dee9878dd38de6d025d983bad4
TheRealANDREWQA <andreicfsteaua@gmail.com>
2023-05-08 00:49:11 +0300

    CORS change

--- src/main/java/com/example/mpp1/Controller/UserController.java
+++ src/main/java/com/example/mpp1/Controller/UserController.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(path = "/api/users")
 public class UserController {
