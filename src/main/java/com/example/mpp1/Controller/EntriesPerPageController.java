cdbc14f4cfaff41cbb6290e78da0f49c5ab426b4
TheRealANDREWQA <andreicfsteaua@gmail.com>
2023-05-13 19:32:22 +0300

    Admin role for entries per page

--- src/main/java/com/example/mpp1/Controller/EntriesPerPageController.java
+++ src/main/java/com/example/mpp1/Controller/EntriesPerPageController.java
@@ -2,6 +2,7 @@ package com.example.mpp1.Controller;
 
 import com.example.mpp1.Model.EntriesPerPage;
 import com.example.mpp1.Service.EntriesPerPageService;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +19,7 @@ public class EntriesPerPageController {
         return service.getEntriesPerPage();
     }
 
+    @RolesAllowed({"ROLE_ADMIN"})
     @PostMapping("/{value}")
     public void set(@PathVariable("value") Integer value) {
         service.setEntriesPerPage(value);
