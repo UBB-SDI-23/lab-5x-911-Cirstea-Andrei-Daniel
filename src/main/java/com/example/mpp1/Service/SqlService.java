9de276f852de15e216e60ab06cb6c9333f13d29b
TheRealANDREWQA <andreicfsteaua@gmail.com>
2023-05-14 10:09:12 +0300

    SqlService detailed logging

--- src/main/java/com/example/mpp1/Service/SqlService.java
+++ src/main/java/com/example/mpp1/Service/SqlService.java
@@ -3,6 +3,8 @@ package com.example.mpp1.Service;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 @Service
@@ -20,11 +22,35 @@ public class SqlService {
             line = reader.readLine();
         }
         if (last_line != null) {
+            String command_success = last_line;
             System.out.println("Last line is " + last_line);
-            return last_line;
+            if (command_success.contentEquals("Success")) {
+                // Verify the log file
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader("/home/andreicfsteaua/lab-5x-911-Cirstea-Andrei-Daniel/logname.txt"));
+                    while ((line = br.readLine()) != null) {
+                        if (!line.contentEquals("INSERT 0 1000") && !line.contentEquals("ALTER SEQUENCE")) {
+                            command_success = "The command executed but it has encountered errors.";
+                            break;
+                        }
+                    }
+                    br.close();
+                }
+                catch (IOException exception) {
+                    command_success = "The command executed successfully but could not read the log file. Reason: " + exception.getMessage();
+                }
+
+                if (command_success.contentEquals("Success")) {
+                    command_success = "The command executed successfully without any errors.";
+                }
+            }
+            else {
+                command_success = "The command failed to execute.";
+            }
+            return command_success;
         }
         else {
-            throw new Exception("Could not read script output");
+            throw new Exception("Could not read script status");
         }
     }
 
