d596e0d2a81065aa0f534d6c028b5dcabb6e70af
TheRealANDREWQA <andreicfsteaua@gmail.com>
2023-05-15 14:40:43 +0300

    Cleanup

--- frontend/mpp/src/components/ServerIP.ts
+++ frontend/mpp/src/components/ServerIP.ts
@@ -2,4 +2,5 @@ export class ServerSettings {
     //public static API_ENDPOINT='/api';
     // public static API_ENDPOINT='http://34.116.216.15';
     public static API_ENDPOINT='https://sdi-2023-car-dealership.crabdance.com';
+    //public static API_ENDPOINT='http://localhost:8080';
 }
--- src/main/java/com/example/mpp1/Service/SqlService.java
+++ src/main/java/com/example/mpp1/Service/SqlService.java
@@ -28,9 +28,6 @@ public class SqlService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    private PlatformTransactionManager transactionManager;
-
     public String ExecuteScript(String script_name) throws Exception {
         if (!user_service.isCurrentUserRole("ROLE_ADMIN")) {
             throw new Exception("Only admins can execute sql scripts. Your current role is " + user_service.getCurrentUser().getRole().getName());
@@ -50,26 +47,7 @@ public class SqlService {
             System.out.println("Read content time " + (load_time - initial_time));
 
             initial_time = load_time;
-
-            TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-            transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-                @Override
-                protected void doInTransactionWithoutResult(TransactionStatus status) {
-                    // Disable auto-commit for the current transaction
-                    try {
-                        jdbcTemplate.getDataSource().getConnection().setAutoCommit(false);
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                    // Execute your SQL statements or queries here
-                    jdbcTemplate.execute(content);
-
-                    // Commit the transaction
-                    status.flush();
-                }
-            });
-
+            jdbcTemplate.execute(content);
             load_time = System.nanoTime();
 
 
@@ -79,96 +57,6 @@ public class SqlService {
         catch (Exception exception) {
             throw new Exception(exception.getMessage());
         }
-
-//        // Specify the root directory path
-//        String rootPath = "dbscripts";
-//
-//        System.out.println("Dbscripts folders");
-//        try (Stream<Path> paths = Files.walk(Paths.get(rootPath))) {
-//            paths.forEach(System.out::println);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        //String command = "dbscripts/execute_sql_script.sh " + script_name;
-//        //String command = "ls dbscripts";
-//        //Process process = Runtime.getRuntime().exec(command);
-//
-//        String filePath = "dbscripts/execute_sql_script.sh";
-//        File file = new File(filePath);
-//
-//        if (file.exists()) {
-//            System.out.println("File exists!");
-//        } else {
-//            System.out.println("File does not exist!");
-//        }
-//
-//
-//        String script_to_execute = "dbscripts/execute_sql_script.sh";
-//        String[] command = {script_to_execute, "insert_car_model.sql"};
-//
-//        ProcessBuilder processBuilder = new ProcessBuilder(command);
-//
-//        try {
-//            Process process = processBuilder.start();
-//            int exitCode = process.waitFor();
-//
-//            if (exitCode == 0) {
-//                // Script execution completed successfully
-//                System.out.println("Script returned successfully.");
-//            } else {
-//                // Script execution encountered an error
-//                System.out.println("Script execution failed with exit code: " + exitCode);
-//            }
-//
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//            String line = reader.readLine();
-//            String last_line = line;
-//            while (line != null) {
-//                System.out.println(line);
-//                last_line = line;
-//                line = reader.readLine();
-//            }
-//            if (last_line != null) {
-//                String command_success = last_line;
-//                System.out.println("Last line is " + last_line);
-//                if (command_success.contentEquals("Success")) {
-//                    // Verify the log file
-//                    try {
-//                        BufferedReader br = new BufferedReader(new FileReader("dbscripts/logname.txt"));
-//                        while ((line = br.readLine()) != null) {
-//                            if (!line.contentEquals("INSERT 0 1000") && !line.contentEquals("ALTER SEQUENCE")
-//                                    && !line.startsWith("DELETE")) {
-//                                System.out.println("Problematic sql line: " + line);
-//                                command_success = "The command executed but it has encountered errors.";
-//                                break;
-//                            }
-//                        }
-//                        br.close();
-//                    }
-//                    catch (IOException exception) {
-//                        command_success = "The command executed successfully but could not read the log file. Reason: " + exception.getMessage();
-//                    }
-//
-//                    if (command_success.contentEquals("Success")) {
-//                        command_success = "The command executed successfully without any errors.";
-//                    }
-//                }
-//                else {
-//                    command_success = "The command failed to execute.";
-//                }
-//                return command_success;
-//            }
-//            else {
-//                throw new Exception("Could not read script status");
-//            }
-//        } catch (IOException e) {
-//            // Error handling for starting the process
-//            throw new Exception(e.getMessage());
-//        } catch (InterruptedException e) {
-//            // Error handling for waiting for the process to complete
-//        }
-
     }
 
 }
