402ea42d37a9ee76095d83e210c383eb2b723219
TheRealANDREWQA <andreicfsteaua@gmail.com>
2023-05-15 10:27:01 +0300

    Command change

--- src/main/java/com/example/mpp1/Service/SqlService.java
+++ src/main/java/com/example/mpp1/Service/SqlService.java
@@ -25,50 +25,76 @@ public class SqlService {
             e.printStackTrace();
         }
 
-        String command = "/bin/bash dbscripts/execute_sql_script.sh " + script_name;
+        //String command = "dbscripts/execute_sql_script.sh " + script_name;
         //String command = "ls dbscripts";
-        Process process = Runtime.getRuntime().exec(command);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = reader.readLine();
-        String last_line = line;
-        while (line != null) {
-            System.out.println(line);
-            last_line = line;
-            line = reader.readLine();
-        }
-        if (last_line != null) {
-            String command_success = last_line;
-            System.out.println("Last line is " + last_line);
-            if (command_success.contentEquals("Success")) {
-                // Verify the log file
-                try {
-                    BufferedReader br = new BufferedReader(new FileReader("dbscripts/logname.txt"));
-                    while ((line = br.readLine()) != null) {
-                        if (!line.contentEquals("INSERT 0 1000") && !line.contentEquals("ALTER SEQUENCE")
-                            && !line.startsWith("DELETE")) {
-                            System.out.println("Problematic sql line: " + line);
-                            command_success = "The command executed but it has encountered errors.";
-                            break;
-                        }
-                    }
-                    br.close();
-                }
-                catch (IOException exception) {
-                    command_success = "The command executed successfully but could not read the log file. Reason: " + exception.getMessage();
-                }
+        //Process process = Runtime.getRuntime().exec(command);
 
+        String command = "/bin/bash";
+        String script_to_execute = "dbscripts/execute_sql_script.sh";
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command, script_to_execute, script_name);
+
+        try {
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+
+            if (exitCode == 0) {
+                // Script execution completed successfully
+                System.out.println("Script returned successfully.");
+            } else {
+                // Script execution encountered an error
+                System.out.println("Script execution failed with exit code: " + exitCode);
+            }
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = reader.readLine();
+            String last_line = line;
+            while (line != null) {
+                System.out.println(line);
+                last_line = line;
+                line = reader.readLine();
+            }
+            if (last_line != null) {
+                String command_success = last_line;
+                System.out.println("Last line is " + last_line);
                 if (command_success.contentEquals("Success")) {
-                    command_success = "The command executed successfully without any errors.";
+                    // Verify the log file
+                    try {
+                        BufferedReader br = new BufferedReader(new FileReader("dbscripts/logname.txt"));
+                        while ((line = br.readLine()) != null) {
+                            if (!line.contentEquals("INSERT 0 1000") && !line.contentEquals("ALTER SEQUENCE")
+                                    && !line.startsWith("DELETE")) {
+                                System.out.println("Problematic sql line: " + line);
+                                command_success = "The command executed but it has encountered errors.";
+                                break;
+                            }
+                        }
+                        br.close();
+                    }
+                    catch (IOException exception) {
+                        command_success = "The command executed successfully but could not read the log file. Reason: " + exception.getMessage();
+                    }
+
+                    if (command_success.contentEquals("Success")) {
+                        command_success = "The command executed successfully without any errors.";
+                    }
                 }
+                else {
+                    command_success = "The command failed to execute.";
+                }
+                return command_success;
             }
             else {
-                command_success = "The command failed to execute.";
+                throw new Exception("Could not read script status");
             }
-            return command_success;
+        } catch (IOException e) {
+            // Error handling for starting the process
+            throw new Exception(e.getMessage());
+        } catch (InterruptedException e) {
+            // Error handling for waiting for the process to complete
         }
-        else {
-            throw new Exception("Could not read script status");
-        }
+
+        return "Unknown error";
     }
 
 }
