e0368fdbb44dcf0290758f31cc3fc9c26a791208
TheRealANDREWQA <andreicfsteaua@gmail.com>
2023-05-28 23:04:10 +0300

    Python change

--- dbscripts/make_prediction.py
+++ dbscripts/make_prediction.py
@@ -3,16 +3,28 @@ import sys
 import tensorflow as tf
 from tensorflow import keras
 
-model = keras.models.load_model('dbscripts/delivery_model')
+import os
 
-# Load the label encoder from file
-with open('dbscripts/delivery_model/label_encoder.pkl', 'rb') as f:
-    label_encoder = pickle.load(f)
+# Get the current directory
+current_dir = os.getcwd()
 
-def perform_tensorflow_operations(country):
-    transformed_country = label_encoder.transform([country])
-    predictions = model.predict([[transformed_country]])
+# List all files in the current directory
+files = os.listdir(current_dir)
+
+# Print the list of files
+for file in files:
+    print(file)
+
+# model = keras.models.load_model('dbscripts/delivery_model')
+
+# # Load the label encoder from file
+# with open('dbscripts/delivery_model/label_encoder.pkl', 'rb') as f:
+#     label_encoder = pickle.load(f)
+
+# def perform_tensorflow_operations(country):
+#     transformed_country = label_encoder.transform([country])
+#     predictions = model.predict([[transformed_country]])
     
-    return str(predictions[0].item())
+#     return str(predictions[0].item())
 
-print(perform_tensorflow_operations(sys.argv[1]))
+# print(perform_tensorflow_operations(sys.argv[1]))
